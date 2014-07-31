c26f4c91c164df3e28eb8b49a8422aa901a54102
cqyy <527068423@qq.com>
2014-07-31 23:24:41 +0800

    initial

---
+++ src/main/java/web/Test.java
@@ -0,0 +1,31 @@
+package web;
+
+
+import org.mortbay.http.HttpContext;
+import org.mortbay.http.HttpServer;
+import org.mortbay.http.SocketListener;
+import org.mortbay.http.handler.ResourceHandler;
+
+
+/**
+ * Created by Kali on 14-7-31.
+ */
+public class Test {
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer();
+        SocketListener listener = new SocketListener();
+        listener.setPort(8081);
+        server.addListener(listener);
+        HttpContext context = new HttpContext();
+        context.setContextPath("/web");
+        context.setResourceBase("./webapp");
+        context.addHandler(new ResourceHandler());
+        server.addContext(context);
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
